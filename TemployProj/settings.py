43a10bf8f79c1ae460c9d5fcab3f7d96b69ef276
Quinton Oliver <quinton.oliver@gmail.com>
2018-02-23 20:00:17 +0000

    Created routes for a home and login page

--- TemployProj/settings.py
+++ TemployProj/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = [
 # Application definition
 
 INSTALLED_APPS = [
+    'temploybackend.apps.TemploybackendConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
