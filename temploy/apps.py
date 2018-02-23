fa28048da21dff54265cea6efefe391ba97d5b62
Chris Merkley <09cmerkley1@gmail.com>
2018-02-23 04:52:54 +0000

    Fixing django

--- temploy/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class TemployConfig(AppConfig):
-    name = 'temploy'
---
+++ temploybackend/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class TemploybackendConfig(AppConfig):
+    name = 'temploybackend'
