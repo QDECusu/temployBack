080c53148cd8740d067ebc703d8cc1e3c325d5dd
Dax <graydax@gmail.com>
2018-02-28 21:54:03 -0700

    Created the job posting model

--- temploybackend/models.py
+++ temploybackend/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class JobListing(models.Model):
+    company_name = models.CharField(max_length=500)
+    job_position = models.CharField(max_length=250)
+    job_phone = models.CharField(max_length=10)
+    job_email = models.EmailField()
+    job_description = models.TextField()
+    job_schedule = models.CharField(max_length=500)
+    job_post_date = models.DateField()
+
